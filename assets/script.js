544f68537af92c831ae9600ebee9b78c266cd3b6
DavidLauren <leroydavid@hotmail.fr>
2023-08-14 16:15:42 +0200

    correction finale

--- assets/script.js
+++ assets/script.js
@@ -1,85 +1,102 @@
-const slides = [
-	{
-		"image": "slide1.jpg",
-		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image": "slide2.jpg",
-		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image": "slide3.jpg",
-		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image": "slide4.png",
-		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+// Encapsulation du code dans une fonction auto-exécutée pour éviter les collisions de variables
+(function() {
+  const slides = [
+    {
+      image: "slide1.jpg", 
+      tagLine: "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+      image: "slide2.jpg",
+      tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+      image: "slide3.jpg",
+      tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+      image: "slide4.png",
+      tagLine: "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
+  ];
 
 
+  const bannerImg = document.querySelector(".banner-img");
+  const bannerText = document.querySelector("#banner p");
+  const arrowRight = document.querySelector(".arrow_right");
+  const arrowLeft = document.querySelector(".arrow_left");
+  const dotsContainer = document.querySelector(".dots");
+  let position = 0;
+  let dots = [];
+  
 
-// Dots
-function generetedDots() {
-	for (let i = 0; i < slides.length; i++) {
-		const dotsParent = document.querySelector(".dots");
-		const dotChild = document.createElement("div");
-		dotChild.classList.add("dot");
-		dotsParent.appendChild(dotChild);
-	}
-}
+  // Fonction pour changer l'image et le texte
+  function changeSlide() {
+    bannerImg.src = "./assets/images/slideshow/" + slides[position].image;
+    bannerText.innerHTML = slides[position].tagLine;
+  }
 
-// Remove dots
-function removeDots() {
-	const dotChild = document.querySelectorAll(".dot");
-	
-	for (let i = 0; i < slides.length; i++) {
-		dotChild[i].classList.remove("dot_selected");
-	}
-	dotChild[slideIndex].classList.add("dot_selected");	
-}
+  // Fonction pour vérifier et ajuster la position
+  function checkPosition() {
+	// Si la position devient supérieure au nombre d'élément de slides alors position = 0 pour boucler le diaporama
+    if (position >= slides.length) {
+      position = 0;
+	//   Et si la position devient négative alors position = au nombre d'élément de slides - 1 pour boucler le diaporama
+    } else if (position < 0) {
+      position = slides.length - 1;
+    }
+  }
 
-// Contenu 
-function generetedSlider(slides) {
-	
-	const imageSlides = document.querySelector(".banner-img");
-	imageSlides.src = "./assets/images/slideshow/" + slides[slideIndex].image;
-	
-	const taglineSlides = document.querySelector("#banner p");
-	taglineSlides.innerHTML = slides[slideIndex].tagLine;
-	
-	console.log(slides[slideIndex].image);
-}
+  // Fonction pour mettre à jour les points avec 2 paramètres dans la méthode forEach
+  //  (dot = point en cours d'itération et index = index de l'élément dans le tableau (auto))
+  function updateDots() {
+    dots.forEach(function(dot, index) {
+      dot.classList.remove("dot_selected");
+      if (index === position) {
+        dot.classList.add("dot_selected");
+      }
+    });
+  }
 
-generetedDots();
+  // Fonction lors du clic sur une flèche (droite ou gauche) position = direction +1 ou -1 et on lance nos fonctions de vérif et changement
+  function ArrowClic(direction) {
+    position += direction;
+    checkPosition();
+    updateDots();
+    changeSlide();
+  }
 
-const test = document.querySelector(".dot");
-test.classList.add("dot_selected");	
+  // Création des points (1 Div avec classe dot pour chaque élément du tableau slides)
+  slides.forEach(function()  {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dotsContainer.appendChild(dot);
+    dots.push(dot);
+  });
 
-var slideIndex = 0;
-const slideShow = slides[slideIndex];
-var flecheGauche = document.querySelector(".arrow_left");
+  // Clic sur la flèche de droite donc je définis direction à 1 dans la fonction ArrowClic
+  arrowRight.addEventListener("click", function() {
+    ArrowClic(1);
+    console.log("Clic sur le bouton de droite");
+    console.log(position);
+  });
 
-// Fleches
-flecheGauche.onclick = function () {
-	if (slideIndex === 0) {
-		slideIndex = slides.length - 1
-	}
-	else slideIndex--;
-	
-	generetedSlider(slides);
-	removeDots();
-}
+  // Clic sur la flèche de gauche donc je définis direction à -1 dans la fonction ArrowClic
+  arrowLeft.addEventListener("click", function() {
+    ArrowClic(-1);
+    console.log("Clic sur le bouton de gauche");
+    console.log(position);
+  });
 
-var flecheDroite = document.querySelector(".arrow_right");
+  // Clic sur un point
+  dots.forEach(function(dot, index) {
+    dot.addEventListener("click", function() {
+      position = index;
+      updateDots();
+      changeSlide();
+    });
+  });
 
-flecheDroite.onclick = function () {
-	if (slideIndex === slides.length - 1) {
-		slideIndex = 0
-	}
-	else slideIndex++;
-	
-	generetedSlider(slides);
-	removeDots();
-}
-
+  // Affichage initial du premier slide pour sécuriser l'affichage de départ sur 0 (position étant égal à 0)
+  changeSlide();
+  updateDots();
+})();
